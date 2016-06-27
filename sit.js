36775b07cc481d3efbd1786ea4beb701fe18f44f
kasia-kasia <kat.kokot92@gmail.com>
2016-06-27 12:26:33 +0200

    update

--- sit.js
+++ sit.js
@@ -6,33 +6,33 @@ document.addEventListener("DOMContentLoaded", function(){
      
      var nextPicture = document.querySelector("#buttonRight");
      var prevPicture = document.querySelector("#buttonLeft");
-     var allElementsList =  document.querySelectorAll(".slider ul li");  
+     var allElements =  document.querySelectorAll(".slider ul li");  
      var index = 0;
      
-     allElementsList[index].classList.add("visible");
+     allElements[index].classList.add("visible");
      
      nextPicture.addEventListener("click", buttonRight);
      prevPicture.addEventListener("click", buttonLeft);
      
     
      function buttonRight(event) {
-         allElementsList[index].classList.remove("visible");
+         allElements[index].classList.remove("visible");
          index++;
          
          if(index >= allElementsList.length) {
              index = 0;
          }
-         allElementsList[index].classList.add("visible");
+         allElements[index].classList.add("visible");
      }
      
      function buttonLeft(event) {
-         allElementsList[index].classList.remove("visible");
+         allElements[index].classList.remove("visible");
          index--;
          
          if(index < 0) {
-             index = allElementsList.length-1;
+             index = allElements.length-1;
          }
-         allElementsList[index].classList.add("visible");
+         allElements[index].classList.add("visible");
      }
 
 
